2599fd56db3322ebc92cbc31a8557a1c5820a127
aloel-ueu <aloel.ku@student.esaunggul.ac.id>
2021-07-31 18:40:16 +0700

    del

--- database/mysql.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-echo 'Starting Database: mysql'$1
-sudo apt-get update
-sudo apt-get install mysql-server -y
-sudo systemctl start mysql
-echo 'Provision mysql'$1 'complete'
--- nginx-HA/prov-web.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-echo 'Starting Provision: web'$1
-sudo apt-get update
-sudo apt-get install -y nginx
-echo "Machine: web"$1 >> /usr/share/nginx/html/index.html
-echo 'Provision web'$1 'complete'
